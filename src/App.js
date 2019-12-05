d071e262f51f82289d506ff42c4b9a8492efacd4
maxim3andr3 <maxim3andr3@gmail.com>
2019-12-05 15:02:02 +0100

    Clear Users From State

--- src/App.js
+++ src/App.js
@@ -22,6 +22,11 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  // Clear users from state
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
+
   render() {
     const { loading, users } = this.state;
 
@@ -29,7 +34,11 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+          />
           <Users loading={loading} users={users} />
         </div>
       </div>
